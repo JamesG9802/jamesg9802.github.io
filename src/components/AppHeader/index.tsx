a016c4835e47c8f0ab9fdee560d72c80e6e78879
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2024-05-14 18:06:15 -0400

    Force screen to scroll up on screen change.

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -19,11 +19,13 @@ type HeaderLinkTextProps = {
 }
 function HeaderLinkText({text, page, link, current_page}: HeaderLinkTextProps) {
     return (
-        <LinkText containerClassName="p-2" to={link}>
-            <Text type="h3" className={page == current_page ? "underline" : "font-semibold"}>
-                {text}
-            </Text>
-        </LinkText>
+        <div onClick={()=>{window.scrollTo(0, 0)}}>
+            <LinkText containerClassName="p-2" to={link}>
+                <Text type="h3" className={page == current_page ? "underline" : "font-semibold"}>
+                    {text}
+                </Text>
+            </LinkText>
+        </div>
     );
 }
 
